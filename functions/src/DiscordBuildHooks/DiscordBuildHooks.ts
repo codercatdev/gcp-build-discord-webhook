7bc29d55b7848a9c811e2cc0679b8909054e09d9
Alex <ajonp@alexs-mbp.lan>
2020-05-02 10:00:51 -0400

    changed name cloud-builds

--- functions/src/DiscordBuildHooks/DiscordBuildHooks.ts
+++ functions/src/DiscordBuildHooks/DiscordBuildHooks.ts
@@ -1,6 +1,6 @@
 import * as functions from 'firebase-functions';
 
-export const gcpBuildTriggerDiscord = functions.pubsub.topic('gcp-build-discord').onPublish(async (pubSubEvent) => {
+export const gcpBuildTriggerDiscord = functions.pubsub.topic('cloud-builds').onPublish(async (pubSubEvent) => {
     const build = JSON.parse(Buffer.from(pubSubEvent.data, 'base64').toString());
 
     console.log(JSON.stringify(build));
