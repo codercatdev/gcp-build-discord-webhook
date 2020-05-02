9fbcf842b53e64f1aecbe69ab6b3cf8184b680a8
Alex <ajonp@alexs-mbp.lan>
2020-05-02 10:23:49 -0400

    added discord send

--- functions/src/DiscordBuildHooks/DiscordBuildHooks.ts
+++ functions/src/DiscordBuildHooks/DiscordBuildHooks.ts
@@ -2,6 +2,83 @@ import * as functions from 'firebase-functions';
 
 export const gcpBuildTriggerDiscord = functions.pubsub.topic('cloud-builds').onPublish(async (pubSubEvent) => {
     const build = JSON.parse(Buffer.from(pubSubEvent.data, 'base64').toString());
+    const status = ['SUCCESS', 'FAILURE', 'INTERNAL_ERROR', 'TIMEOUT'];
+    if (status.indexOf(build.status) === -1) {
+        console.log('Status not found');
+    }
 
-    console.log(JSON.stringify(build));
+    try {
+        const msg = createBuildMessage(build);
+        await sendDiscordBuildPost(msg);
+    } catch (err) {
+        console.log(err);
+    }
 });
+
+const sendDiscordBuildPost = async (body: object) => {
+    console.log(`Calling Discord wtih`, JSON.stringify(body));
+    const result = await fetch(functions.config().discord.build_hook, {
+        body: JSON.stringify(body),
+        method: 'POST',
+        headers: { "Content-Type": "application/json" }
+    });
+    console.log(result.json());
+}
+
+const createBuildMessage = (build: GoogleCloudBuild) => {
+    const embeds: Embed[] = [];
+    build.steps.forEach(step => {
+        embeds.push({
+            title: step.name,
+            description:
+                `${step.entrypoint} ${step.args.join(' ')}` +
+                `took ${(<any>new Date(step.timing.endTime) - <any>new Date(step.timing.startTime)) * .001}` +
+                `and ${step.status}`,
+            color: build.status === 'FAILURE' ? 16714507 : 6618931
+        });
+    });
+    return {
+        content: `Build ${build.id} for project ${build.projectId} was a ${build.status} ` +
+            `took ${(<any>new Date(build.finishTime) - <any>new Date(build.startTime)) * .001}`,
+        tts: build.status === 'FAILURE' ? true : false,
+        embeds
+    }
+}
+
+export interface Embed {
+    title?: string;
+    description?: string;
+    color?: number;
+}
+
+
+export interface GoogleCloudBuild {
+    id: string;
+    projectId: string;
+    status: string;
+    steps: Step[];
+    createTime: Date;
+    startTime: Date;
+    finishTime: Date;
+    buildTriggerId: string;
+    options: Options;
+}
+
+export interface Options {
+    substitutionOption?: string;
+    logging?: string;
+}
+
+export interface Step {
+    name: string;
+    args: string[];
+    entrypoint: string;
+    timing: Timing;
+    pullTiming: Timing;
+    status: string;
+}
+
+export interface Timing {
+    startTime: Date;
+    endTime: Date;
+}
